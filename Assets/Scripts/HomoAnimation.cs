28c8a94ad4dabc05d290f1ef7dc3477ae234f998
Yuri <ura_rat@mail.ru>
2021-06-08 19:39:37 +0300

    Анимация бега

---
+++ Assets/Scripts/HomoAnimation.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class HomoAnimation : MonoBehaviour
+{
+    public Mesh[] frames = new Mesh[2];
+    //public Mesh[] framesJump = new Mesh[1];
+    public float animationSpeed = .1f;
+    public static bool jumpAnim = false, runAnim = true;
+    private float animationStartTime;
+    private int currentFrame;
+    //private int currentFrame2;
+
+    void Start()
+    {
+        currentFrame = 0;
+       // currentFrame2 = 0;
+        animationStartTime = Time.time;
+        UpdateMesh();
+    }
+
+    void Update()
+    {
+        currentFrame = Mathf.FloorToInt((Time.time - animationStartTime) / animationSpeed);
+        currentFrame = currentFrame % frames.Length;
+      //  currentFrame2 = Mathf.FloorToInt((Time.time - animationStartTime) / animationSpeed);
+       // currentFrame2 = currentFrame2 % framesJump.Length;
+        UpdateMesh();
+    }
+
+    void UpdateMesh()
+    {
+        GetComponent<MeshFilter>().mesh = frames[currentFrame];
+        /*if (runAnim)
+        {
+            GetComponent<MeshFilter>().mesh = frames[currentFrame];
+        }
+        else if (jumpAnim)
+        {
+            GetComponent<MeshFilter>().mesh = framesJump[currentFrame2];
+        }*/
+    }
+}
